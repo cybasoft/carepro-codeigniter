8be5ce36161f503032d659d97be988dc0081a941
jgmuchiri <jgmuchiri@gmail.com>
2018-01-03 00:27:51 -0500

    Update translations

--- application/language/english/parents_lang.php
+++ application/language/english/parents_lang.php
@@ -6,6 +6,9 @@
  * @link        https://amdtllc.com
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  */
+
+$lang['parent'] = 'Parent';
+$lang['parents'] = 'Parents';
 $lang['no_children_notice'] = 'There are children associated with your profile.
             If this is a mistake and your child has been registered,
             please contact a staff member.';
