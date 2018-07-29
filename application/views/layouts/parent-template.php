1de6220b152604198c46f32c4a73de0b8a7bf296
jgmuchiri <jgmuchiri@gmail.com>
2018-07-29 00:45:14 -0400

    Added tawk.to to parents

--- application/views/layouts/parent-template.php
+++ application/views/layouts/parent-template.php
@@ -231,5 +231,21 @@
         e.preventDefault();
     });
 </script>
+
+<?php if(!empty(get_option('tawkto_embed_url'))): ?>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='<?php echo get_option('tawkto_embed_url'); ?>';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
+<?php endif; ?>
 </body>
 </html>
