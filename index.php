48ad2e6124eed832335f1e9a5392d6d444b27bce
jgmuchiri <4142133+jgmuchiri@users.noreply.github.com>
2019-09-19 23:56:08 -0400

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 //development, staging, production
 
-$APP_ENV = 'development';
+$APP_ENV = 'production';
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT ($APP_ENV)
@@ -109,7 +109,7 @@ $view_folder = '';
 // $routing['controller'] = '';
 
 // The controller function you wish to be called.
-// $routing['function']	= '';
+// $routing['function'] = '';
 
 
 /*
@@ -150,10 +150,10 @@ if (($_temp = realpath($system_path)) !== FALSE) {
 } else {
     // Ensure there's a trailing slash
     $system_path = strtr(
-        rtrim($system_path, '/\\'),
-        '/\\',
-        DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-    ) . DIRECTORY_SEPARATOR;
+            rtrim($system_path, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
+        ) . DIRECTORY_SEPARATOR;
 }
 
 // Is the system path correct?
@@ -193,10 +193,10 @@ if (is_dir($application_folder)) {
     }
 } elseif (is_dir(BASEPATH . $application_folder . DIRECTORY_SEPARATOR)) {
     $application_folder = BASEPATH . strtr(
-        trim($application_folder, '/\\'),
-        '/\\',
-        DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-    );
+            trim($application_folder, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
+        );
 } else {
     header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
     echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: ' . SELF;
@@ -220,10 +220,10 @@ if (!isset($view_folder[0]) && is_dir(APPPATH . 'views' . DIRECTORY_SEPARATOR)) 
     }
 } elseif (is_dir(APPPATH . $view_folder . DIRECTORY_SEPARATOR)) {
     $view_folder = APPPATH . strtr(
-        trim($view_folder, '/\\'),
-        '/\\',
-        DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
-    );
+            trim($view_folder, '/\\'),
+            '/\\',
+            DIRECTORY_SEPARATOR . DIRECTORY_SEPARATOR
+        );
 } else {
     header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
     echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: ' . SELF;
